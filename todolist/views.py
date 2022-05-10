2353fe2691cbe5b70e598ea2ab0bec35db4d96f3
Fisher <alexfisher13@yandex.ru>
2022-05-10 14:08:44 +0300

    form to add task

--- todolist/views.py
+++ todolist/views.py
@@ -4,6 +4,9 @@ from todos.models import Task
 
 
 def main_page(request):
+    task_text = request.POST.get('new_task_text', 'new task')
+    new_task = Task(text=task_text, is_done=False)
+    new_task.save()
     tasks = Task.objects.all()
     return render(request, './index.html', {
         'text': 'Список заданий',
