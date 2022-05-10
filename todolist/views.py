8865c4815c6cd96181af605d2606180725548cb9
Fisher <alexfisher13@yandex.ru>
2022-05-10 13:01:05 +0300

    print tasks list to page

--- todolist/views.py
+++ todolist/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from todos.models import Task
+
 
 def main_page(request):
+    tasks = Task.objects.all()
     return render(request, './index.html', {
-        'text': 'Список заданий'
+        'text': 'Список заданий',
+        'tasks': tasks
     })
